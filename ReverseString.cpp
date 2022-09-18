b55dad4ae9bd7a7135c3a845841ce8453ccb3479
Rajan Sharma <82056580+Immortal-codes@users.noreply.github.com>
2022-09-18 15:41:51 +0530

    Add files via upload

---
+++ ReverseString.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string.h>
+#include<algorithm>
+
+using namespace std;
+int main()
+{
+string str;
+str="laguage";
+reverse(str.begin(), str.end());
+cout << str << endl;
+}
